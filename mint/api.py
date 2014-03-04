be52955835d7708abc25937244e8c1464d6ceaf7
barofsoap <fishkid@yahoo.com>
2014-03-04 15:10:17 +0100

    Fix for Mint update; removed pyquery dep (pure json now)

--- mint/api.py
+++ mint/api.py
@@ -1,42 +1,41 @@
 import json
-from pyquery import PyQuery as pq
 import requests
 
 def get_accounts(email, password):
     # 1: Login.
     session = requests.Session()
     data = {"username": email, "password": password, "task": "L", "nextPage": ""}
-    response = session.post("https://wwws.mint.com/loginUserSubmit.xevent", data=data).text
-    if "javascript-token" not in response.lower():
+    headers = {'accept': 'application/json'}
+    response = json.loads(session.post("https://wwws.mint.com/loginUserSubmit.xevent", data=data, headers=headers).text)
+    if not response["sUser"]["token"]:
         raise Exception("Mint.com login failed")
 
     # 2: Grab token.
-    d = pq(response.encode("utf-8"))
-    token = d("input#javascript-token")[0].value
+    token = response["sUser"]["token"]
 
     # 3. Issue service request.
     request_id = "115485" # magic number? random number?
     data = {"input": json.dumps([
         {"args": {
             "types": [
-                "BANK", 
-                "CREDIT", 
-                "INVESTMENT", 
-                "LOAN", 
-                "MORTGAGE", 
-                "OTHER_PROPERTY", 
-                "REAL_ESTATE", 
-                "VEHICLE", 
+                "BANK",
+                "CREDIT",
+                "INVESTMENT",
+                "LOAN",
+                "MORTGAGE",
+                "OTHER_PROPERTY",
+                "REAL_ESTATE",
+                "VEHICLE",
                 "UNCLASSIFIED"
             ]
-        }, 
-        "id": request_id, 
-        "service": "MintAccountService", 
+        },
+        "id": request_id,
+        "service": "MintAccountService",
         "task": "getAccountsSorted"}
     ])}
-    response = session.post("https://wwws.mint.com/bundledServiceController.xevent?token="+token, data=data)
-    response = json.loads(response.text)["response"]
-    accounts = response[request_id]["response"]
+    response = session.post("https://wwws.mint.com/bundledServiceController.xevent?token="+token, data=data, headers=headers)
+    response = json.loads(response.text)
+    accounts = response["response"][request_id]["response"]
     return accounts
 
 if __name__ == "__main__":
