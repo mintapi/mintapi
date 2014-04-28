4a447a823f73fdc58ace75ee1d777ad04d025eb7
Mike Cronce <floppie@quadra-tec.net>
2014-04-28 18:56:37 -0400

    api.py:  Turned into a class

--- mint/api.py
+++ mint/api.py
@@ -1,54 +1,66 @@
 import json
 import requests
 
-def get_accounts(email, password):
-    # 1: Login.
-    session = requests.Session()
+class Mint:
+	headers = {"accept": "application/json"}
+	request_id = 42
+	session = None
+	token = None
 
-    if session.get("https://wwws.mint.com/login.event?task=L").status_code != requests.codes.ok:
-        raise Exception("Failed to load Mint main page '{}'".format(Mint.START_URL))
-    
-    data = {"username": email, "password": password, "task": "L", "browser": "firefox", "browserVersion": "27", "os": "linux"}
-    headers = {"accept": "application/json"}
-    response = session.post("https://wwws.mint.com/loginUserSubmit.xevent", data=data, headers=headers).text
-    if "token" not in response:
-        raise Exception("Mint.com login failed[1]")
+	def login_and_get_token(self, email, password):
+		# 0: Check to see if we're already logged in.
+		if(self.token != None):
+			return
 
-    response = json.loads(response)
-    if not response["sUser"]["token"]:
-        raise Exception("Mint.com login failed[2]")
+		# 1: Login.
+		self.session = requests.Session()
+		if self.session.get("https://wwws.mint.com/login.event?task=L").status_code != requests.codes.ok:
+			raise Exception("Failed to load Mint main page '{}'".format(Mint.START_URL))
 
-    # 2: Grab token.
-    token = response["sUser"]["token"]
+		data = {"username": email, "password": password, "task": "L", "browser": "firefox", "browserVersion": "27", "os": "linux"}
+		response = self.session.post("https://wwws.mint.com/loginUserSubmit.xevent", data=data, headers=self.headers).text
+		if "token" not in response:
+			raise Exception("Mint.com login failed[1]")
 
-    # 3. Issue service request.
-    request_id = "42" # magic number? random number?
-    data = {"input": json.dumps([
-        {"args": {
-            "types": [
-                "BANK", 
-                "CREDIT", 
-                "INVESTMENT", 
-                "LOAN", 
-                "MORTGAGE", 
-                "OTHER_PROPERTY", 
-                "REAL_ESTATE", 
-                "VEHICLE", 
-                "UNCLASSIFIED"
-            ]
-        }, 
-        "id": request_id, 
-        "service": "MintAccountService", 
-        "task": "getAccountsSorted"
-        #"task": "getAccountsSortedByBalanceDescending"
-        }
-    ])}
-    response = session.post("https://wwws.mint.com/bundledServiceController.xevent?legacy=false&token="+token, data=data, headers=headers).text
-    if request_id not in response:
-        raise Exception("Could not parse account data: " + response)
-    response = json.loads(response)
-    accounts = response["response"][request_id]["response"]
-    return accounts
+		response = json.loads(response)
+		if not response["sUser"]["token"]:
+			raise Exception("Mint.com login failed[2]")
+
+		# 2: Grab token.
+		self.token = response["sUser"]["token"]
+
+	def get_accounts(self, email, password):
+		# 1: Login
+		self.login_and_get_token(email, password)
+
+		# 2: Issue servie request.
+		request_id = "42" # magic number? random number?
+		data = {"input": json.dumps([
+			{"args": {
+				"types": [
+					"BANK", 
+					"CREDIT", 
+					"INVESTMENT", 
+					"LOAN", 
+					"MORTGAGE", 
+					"OTHER_PROPERTY", 
+					"REAL_ESTATE", 
+					"VEHICLE", 
+					"UNCLASSIFIED"
+				]
+			}, 
+			"id": request_id, 
+			"service": "MintAccountService", 
+			"task": "getAccountsSorted"
+			#"task": "getAccountsSortedByBalanceDescending"
+			}
+		])}
+		response = self.session.post("https://wwws.mint.com/bundledServiceController.xevent?legacy=false&token="+self.token, data=data, headers=self.headers).text
+		if request_id not in response:
+			raise Exception("Could not parse account data: " + response)
+		response = json.loads(response)
+		accounts = response["response"][request_id]["response"]
+		return accounts
 
 if __name__ == "__main__":
     import getpass, sys
@@ -63,5 +75,7 @@ if __name__ == "__main__":
         email = input("Mint email: ")
         password = getpass.getpass("Password: ")
 
-    accounts = get_accounts(email, password)
+    mint = Mint()
+
+    accounts = mint.get_accounts(email, password)
     print(json.dumps(accounts, indent=2))
