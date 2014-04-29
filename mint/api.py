7aa298b186baa597952e8a44c173a19934620f31
Mike Cronce <floppie@quadra-tec.net>
2014-04-29 00:10:26 -0400

    Mint class:  + populate_extended_account_detail() and supporting functions.  This retrieves the stuff that would be displayed on an account's detail page - total fees, available/total credit, etc

--- mint/api.py
+++ mint/api.py
@@ -1,9 +1,13 @@
+import datetime
 import json
+import random
 import requests
+import time
+import xmltodict
 
 class Mint:
 	headers = {"accept": "application/json"}
-	request_id = 42
+	request_id = 42 # magic number? random number?
 	session = None
 	token = None
 
@@ -12,6 +16,22 @@ class Mint:
 		mint = Mint()
 		mint.login_and_get_token(email, password)
 		return mint
+	# }}}
+
+	@classmethod
+	def get_rnd(_): # {{{
+		return str(int(time.mktime(datetime.datetime.now().timetuple()))) + str(random.randrange(999)).zfill(3)
+	# }}}
+
+	@classmethod
+	def parse_float(_, string): # {{{
+		for bad_char in ['$', ',', '%']:
+			string = string.replace(bad_char, '')
+
+		try:
+			return float(string)
+		except ValueError:
+			return None
 	# }}}
 
 	def login_and_get_token(self, email, password): # {{{
@@ -37,13 +57,13 @@ class Mint:
 		self.token = response["sUser"]["token"]
 	# }}}
 
-	def get_accounts(self, email = None, password = None): # {{{
+	def get_accounts(self, email = None, password = None, get_detail = False): # {{{
 		# 1: Login
 		if(email != None and password != None):
 			self.login_and_get_token(email, password)
 
-		# 2: Issue servie request.
-		request_id = "42" # magic number? random number?
+		# 2: Issue service request.
+		req_id = str(self.request_id)
 		data = {"input": json.dumps([
 			{"args": {
 				"types": [
@@ -58,17 +78,64 @@ class Mint:
 					"UNCLASSIFIED"
 				]
 			}, 
-			"id": request_id, 
+			"id": req_id, 
 			"service": "MintAccountService", 
 			"task": "getAccountsSorted"
 			#"task": "getAccountsSortedByBalanceDescending"
 			}
 		])}
 		response = self.session.post("https://wwws.mint.com/bundledServiceController.xevent?legacy=false&token="+self.token, data=data, headers=self.headers).text
-		if request_id not in response:
+		self.request_id = self.request_id + 1
+		if req_id not in response:
 			raise Exception("Could not parse account data: " + response)
 		response = json.loads(response)
-		accounts = response["response"][request_id]["response"]
+		accounts = response["response"][req_id]["response"]
+		if(get_detail):
+			accounts = self.populate_extended_account_detail(accounts)
+		return accounts
+	# }}}
+
+	def populate_extended_account_detail(self, accounts): # {{{
+		# I can't find any way to retrieve this information other than by
+		# doing this stupid one-call-per-account to listTransactions.xevent
+		# and parsing the HTML snippet :(
+		for account in accounts:
+			headers = self.headers
+			headers['Referer'] = 'https://wwws.mint.com/transaction.event?accountId=' + str(account['id'])
+			response = json.loads(self.session.get(
+				'https://wwws.mint.com/listTransaction.xevent?accountId=' + str(account['id']) + '&queryNew=&offset=0&comparableType=8&acctChanged=T&rnd=' + Mint.get_rnd(),
+				headers = headers
+			).text)
+			xml = '<div>' + response['accountHeader'] + '</div>'
+			xml = xml.replace('&#8211;', '-')
+			xml = xmltodict.parse(xml)
+
+			account['availableMoney'] = None
+			account['totalFees'] = None
+			account['totalCredit'] = None
+			account['nextPaymentAmount'] = None
+			account['nextPaymentDate'] = None
+
+			xml = xml['div']['div'][1]['table']
+			if(not 'tbody' in xml):
+				continue
+			xml = xml['tbody']
+			table_type = xml['@id']
+			xml = xml['tr'][1]['td']
+
+			if(table_type == 'account-table-bank'):
+				account['availableMoney'] = Mint.parse_float(xml[1]['#text'])
+				account['totalFees'] = Mint.parse_float(xml[3]['a']['#text'])
+			elif(table_type == 'account-table-credit'):
+				account['availableMoney'] = Mint.parse_float(xml[1]['#text'])
+				account['totalCredit'] = Mint.parse_float(xml[2]['#text'])
+				account['totalFees'] = Mint.parse_float(xml[4]['a']['#text'])
+			elif(table_type == 'account-table-loan'):
+				account['nextPaymentAmount'] = Mint.parse_float(xml[1]['#text'])
+				account['nextPaymentDate'] = xml[2]['#text']
+			elif(table_type == 'account-type-investment'):
+				account['totalFees'] = Mint.parse_float(xml[2]['a']['#text'])
+
 		return accounts
 	# }}}
 
@@ -87,5 +154,6 @@ if __name__ == "__main__":
 
     mint = Mint.create(email, password)
 
-    accounts = mint.get_accounts()
-    print(json.dumps(accounts, indent=2))
+    accounts = mint.get_accounts(get_detail = True)
+    print(json.dumps(accounts))
+
