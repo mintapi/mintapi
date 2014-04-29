b6454de27dc280c60318298151fe935f86d77151
Michael Rooney <mrooney.github@rowk.com>
2014-04-28 19:24:08 -0500

    setup uses pandoc to make a nice long_description rst from readme.md

--- setup.py
+++ setup.py
@@ -1,9 +1,17 @@
 # new version to pypi => python setup.py sdist upload
 from setuptools import setup
 
+try:
+    from pypandoc import convert
+    read_md = lambda f: convert(f, 'rst')
+except ImportError:
+    print("warning: pypandoc module not found, could not convert Markdown to RST")
+    read_md = lambda f: open(f, 'r').read()
+
 setup(
     name='mintapi',
     description='a screen-scraping API for Mint.com',
+    long_description=read_md('README.md'),
     version='1.2',
     packages=['mintapi'],
     scripts=['bin/mintapi'],
