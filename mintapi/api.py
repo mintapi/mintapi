ce461ff0226d5870e499356ed1688c59e753753b
Mike Cronce <floppie@quadra-tec.net>
2014-11-16 11:53:10 -0500

    Fixed executable backward-compatibility through the use of command-line options; see mintapi --help for more information

--- mintapi/api.py
+++ mintapi/api.py
@@ -246,12 +246,15 @@ def get_accounts(email, password, get_detail = False):
 	mint = Mint.create(email, password)
 	return mint.get_accounts(get_detail = get_detail)
 
-def print_accounts(accounts):
+def make_accounts_presentable(accounts):
 	for account in accounts:
 		for k, v in account.items():
 			if isinstance(v, datetime.datetime):
 				account[k] = repr(v)
-	print(json.dumps(accounts, indent = 2))
+	return accounts
+
+def print_accounts(accounts):
+	print(json.dumps(make_accounts_presentable(accounts), indent = 2))
 
 def get_budgets(email, password):
 	mint = Mint.create(email, password)
@@ -263,22 +266,43 @@ def initiate_account_refresh(email, password):
 
 def main():
 	import getpass
+	import optparse
 	import sys
 
-	# Handle Python 3's raw_input change.
-	try:
-		input = raw_input
-	except NameError:
-		pass
+	# Parse command-line arguments {{{
+	cmdline = optparse.OptionParser(usage = 'usage: %prog [options] email password')
+	cmdline.add_option('--accounts', action = 'store_true', dest = 'accounts', default = False, help = 'Retrieve account information (default if nothing else is specified)')
+	cmdline.add_option('--budgets', action = 'store_true', dest = 'budgets', default = False, help = 'Retrieve budget information')
+	cmdline.add_option('--extended-accounts', action = 'store_true', dest = 'accounts_ext', default = False, help = 'Retrieve extended account information (slower, implies --accounts)')
 
-	if len(sys.argv) >= 3:
-		email, password = sys.argv[1:]
+	(options, args) = cmdline.parse_args()
+	
+	if(len(args) >= 2):
+		(email, password) = args[0:1]
 	else:
 		email = input("Mint email: ")
 		password = getpass.getpass("Password: ")
 
-	accounts = get_accounts(email, password)
-	print_accounts(accounts)
+	if(options.accounts_ext):
+		options.accounts = True
+
+	if(not (options.accounts or options.budgets)):
+		options.accounts = True
+	# }}}
+
+	mint = Mint.create(email, password)
+	
+	data = None
+	if(options.accounts and options.budgets):
+		accounts = make_accounts_presentable(mint.get_accounts(get_detail = options.accounts_ext))
+		budgets = mint.get_budgets()
+		data = {'accounts' : accounts, 'budgets' : budgets}
+	elif(options.budgets):
+		data = mint.get_budgets()
+	elif(options.accounts):
+		data = make_accounts_presentable(mint.get_accounts(get_detail = options.accounts_ext))
+	
+	print(json.dumps(data, indent = 2))
 
 if __name__ == "__main__":
 	main()
