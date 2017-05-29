854205e97a666a570b3d4270ef40cc8583ca8981
Ramon Felciano <felciano@yahoo.com>
2017-05-28 20:43:27 -0700

    Made presentation format a parameter and cleaned up formatting
    - Made presentation format a parameter
    - Cleaned up code to be PEP8 compliant

--- mintapi/api.py
+++ mintapi/api.py
@@ -586,15 +586,17 @@ def get_net_worth(email, password):
     account_data = mint.get_accounts()
     return mint.get_net_worth(account_data)
 
+
 DATE_FMT = '%Y-%m-%d'
 ISO8601_FMT = '%Y-%m-%dT%H:%M:%SZ'
 EXCEL_FMT = '%Y-%m-%d %H:%M:%S'
 
-def make_accounts_presentable(accounts):
+
+def make_accounts_presentable(accounts, presentable_format = EXCEL_FMT):
     for account in accounts:
         for k, v in account.items():
             if isinstance(v, datetime):
-                account[k] = v.strftime(EXCEL_FMT)
+                account[k] = v.strftime(presentable_format)
     return accounts
 
 
