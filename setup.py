e958bd4ea6856d88921e87416fff886584e2bc9e
Jaron Lundwall <13423952+jaron-l@users.noreply.github.com>
2021-11-09 21:07:20 +0000

    fix: :bug: Set minimum python version to 3.6
    * remove python2 code
    * remove pandas import checking as it is a dependency
    
    refs: #334

--- setup.py
+++ setup.py
@@ -18,6 +18,7 @@ setuptools.setup(
     author_email='mrooney.mintapi@rowk.com',
     url='https://github.com/mintapi/mintapi',
     install_requires=['configargparse', 'future', 'mock', 'oathtool', 'pandas>=1.0', 'requests', 'selenium', 'selenium-requests>=1.3.3', 'xmltodict'],
+    python_requires=">=3.6",
     entry_points=dict(
         console_scripts=[
             'mintapi = mintapi.api:main',
