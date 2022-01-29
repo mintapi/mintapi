mintapi/mintapi

commit 703937728ba58225e234ffce9dd78f96c9559cdc

Include investment transactions for csv export (#384)
* Include investment transactions for csv export

Co-authored-by: Matthew Burke <burkematthewjoseph@gmail.com>

mintapi/api.py
@@ -426,7 +426,7 @@ class Mint(object):
             # Specifying accountId=0 causes Mint to return investment
             # transactions as well.  Otherwise they are skipped by
             # default.
-            if id > 0 or include_investment:
+            if __include_investments_with_transactions(id, include_investment):
                 params["accountId"] = id
             if include_investment:
                 params["task"] = "transactions"
@@ -514,7 +514,9 @@ class Mint(object):
         # default.
 
         params = {
-            "accountId": acct if acct > 0 else None,
+            "accountId": acct
+            if __include_investments_with_transactions(acct, include_investment)
+            else None,
             "startDate": convert_date_to_string(convert_mmddyy_to_datetime(start_date)),
             "endDate": convert_date_to_string(convert_mmddyy_to_datetime(end_date)),
         }
@@ -833,6 +835,9 @@ class Mint(object):
                     }
                 )
         return utilization
+
+    def __include_investments_with_transactions(id, include_investment):
+        return id > 0 or include_investment
 
 
 def get_accounts(email, password, get_detail=False):

commit 55c36ece1e60eb01b7dc97da2ce6b60a5a999aac

Fixing regression in PR #384

mintapi/api.py
@@ -426,7 +426,7 @@ class Mint(object):
             # Specifying accountId=0 causes Mint to return investment
             # transactions as well.  Otherwise they are skipped by
             # default.
-            if __include_investments_with_transactions(id, include_investment):
+            if self._include_investments_with_transactions(id, include_investment):
                 params["accountId"] = id
             if include_investment:
                 params["task"] = "transactions"
@@ -515,7 +515,7 @@ class Mint(object):
 
         params = {
             "accountId": acct
-            if __include_investments_with_transactions(acct, include_investment)
+            if self._include_investments_with_transactions(acct, include_investment)
             else None,
             "startDate": convert_date_to_string(convert_mmddyy_to_datetime(start_date)),
             "endDate": convert_date_to_string(convert_mmddyy_to_datetime(end_date)),
@@ -836,7 +836,7 @@ class Mint(object):
                 )
         return utilization
 
-    def __include_investments_with_transactions(id, include_investment):
+    def _include_investments_with_transactions(self, id, include_investment):
         return id > 0 or include_investment
 
 

tests/test_driver.py
@@ -400,6 +400,13 @@ class MintApiTests(unittest.TestCase):
         arguments = parse_arguments_file(config_file)
         self.assertEqual(mintapi.cli.validate_file_extensions(arguments), None)
 
+    def test_include_investments_with_transactions(self):
+        mint = mintapi.Mint()
+        self.assertFalse(mint._include_investments_with_transactions(0, False))
+        self.assertTrue(mint._include_investments_with_transactions(0, True))
+        self.assertTrue(mint._include_investments_with_transactions(1, False))
+        self.assertTrue(mint._include_investments_with_transactions(1, True))
+
 
 def write_extended_transactions_file():
     config_file = tempfile.NamedTemporaryFile(mode="wt")