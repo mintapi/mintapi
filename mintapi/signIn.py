b9c0a102dccaaf9576476b92e26a4c53a4512862
Matthew Burke <burkematthewjoseph@gmail.com>
2022-03-05 10:04:19 -0600

    Return the unsupported mfa_method in the runtime error message

--- mintapi/signIn.py
+++ mintapi/signIn.py
@@ -358,7 +358,7 @@ def sign_in(
             handle_same_page_username_password(driver, email, password)
         # try to enter in credentials if username and password are on different pages
         except (
-            ElementNotInteractableException, 
+            ElementNotInteractableException,
             ElementNotVisibleException,
             NoSuchElementException,
         ):
@@ -542,7 +542,9 @@ def set_mfa_method(driver, mfa_method):
         )
         mfa_method = mfa_result[MFA_METHOD_LABEL]
     except (NoSuchElementException, ElementNotInteractableException):
-        raise RuntimeError("The Multifactor Method supplied is not available.")
+        raise RuntimeError(
+            "The Multifactor Method {} supplied is not available.".format(mfa_method)
+        )
     return mfa_token_input, mfa_token_button, mfa_method
 
 
