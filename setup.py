0df6e82796acb879d60eeec116efeef76a3b3782
Michael Rooney <mrooney@users.noreply.github.com>
2022-05-17 22:26:30 -0500

    bump to 2.1 with recent improvements (#474)
    * bump version
    
    * Update CHANGELOG.md

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     description="a screen-scraping API for Mint.com",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    version="2.0",
+    version="2.1",
     packages=["mintapi"],
     license="The MIT License",
     author="Michael Rooney",
