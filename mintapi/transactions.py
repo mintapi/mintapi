f14997ac9e204baef11f5a0a713387d4cfcbb97a
Calvin <calvinchancan@gmail.com>
2023-04-10 00:26:26 -0400

    Add update_transaction_data endpoint method

--- mintapi/transactions.py
+++ mintapi/transactions.py
@@ -3,6 +3,9 @@ Transactions helper classes
 """
 
 from dataclasses import dataclass
+from enum import Enum
+from typing import List
+
 from mintapi.filters import SearchFilter, DateFilter
 
 
@@ -32,3 +35,64 @@ class TransactionRequest:
             "limit": self.limit,
             "offset": self.offset,
         }
+
+
+@dataclass
+class TransactionUpdateRequest:
+    class TransactionType(Enum):
+        CashAndCreditTransaction = "CashAndCreditTransaction"
+        InvestmentTransaction = "InvestmentTransaction"
+        LoanTransaction = "LoanTransaction"
+
+    """
+    Helper class to construct a transaction update request payload.
+
+    Parameters
+    ----------
+    type : TransactionType
+        The type of transaction to update.
+    description : str, optional
+        Optional description to assign to the transaction.
+    notes : str, optional
+        Optional notes to associate with the transaction.
+    category_id : str, optional
+        Optional category ID to assign to the transaction.
+    tag_ids : List[str], optional
+        Optional list of tag IDs to assign to the transaction.
+
+    Raises
+    ------
+    ValueError
+        If the transaction type is not one of the allowed values in the `TransactionUpdateRequest.TransactionType` enum.
+    """
+    type: TransactionType
+    description: str = None
+    notes: str = None
+    category_id: str = None
+    tag_ids: List[str] = None
+
+    def __post_init__(self):
+        if isinstance(self.type, str):
+            assert hasattr(self.TransactionType, self.type)
+        else:
+            raise ValueError(
+                "Transaction Type must be one of allowed values in enum `TransactionUpdateRequest.TransactionType"
+            )
+
+    def to_dict(self):
+        payload = {
+            "type": self.type,
+        }
+        if self.description is not None:
+            payload["description"] = self.description
+        if self.notes is not None:
+            payload["notes"] = self.notes
+        if self.category_id is not None:
+            payload["category"] = {
+                "id": self.category_id,
+            }
+        if self.tag_ids is not None:
+            payload["tagData"] = {
+                "tags": [{"id": tag_id} for tag_id in self.tag_ids],
+            }
+        return payload
